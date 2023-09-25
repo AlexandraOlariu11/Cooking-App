da7c732035e7c8100ed2bd6e128367aff68a74b3
AlexandraOlariu11 <elenaalexandraolariu@gmail.com>
2023-09-25 22:02:54 +0300

    I have developed the cooking application

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./assets/css/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import { MealPage } from "./pages/MealPage";
+import FavoritesMealsPage from "./pages/FavoritesMealsPage";
+
+function App() {
+  return (
+    <div className="container">
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/meals" element={<HomePage />} />
+          <Route path="/meals/:idMeal" element={<MealPage />} />
+          <Route path="/favorites" element={<FavoritesMealsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
